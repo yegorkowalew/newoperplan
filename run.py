322986b9c3814a39391247e259a3517b57418080
yegorkowalew <kowalew.backup@gmail.com>
2020-01-31 18:55:13 +0200

    Генерация трех готовых файлов

--- run.py
+++ run.py
@@ -5,14 +5,15 @@
 """
 from readready import readyReadFile
 from readservicenote import serviceNoteReadFile
-from settings import READY_FILE, SN_FILE
+from readindocument import worker
+from settings import READY_FILE, SN_FILE, IN_DOCUMENT_FILE, IN_DOCUMENT_FOLDER
 
 if __name__ == "__main__":
     readyDf = readyReadFile(READY_FILE)
+    readyDf.to_excel("Ready.xlsx")
+
     serviceNoteDf = serviceNoteReadFile(SN_FILE)
-    print(serviceNoteDf)
-    serviceNoteDf.to_excel("output1.xlsx")
-# readyReadFile(READY_FILE)
-# readyReadFile(READY_FILE).to_excel("output.xlsx")
-# serviceNoteReadFile(SN_FILE).to_excel("output1.xlsx")
-# serviceNoteReadFile(SN_FILE)
+    serviceNoteDf.to_excel("Service_Notes.xlsx")
+
+    inDocumentDf = worker(IN_DOCUMENT_FILE, IN_DOCUMENT_FOLDER)
+    inDocumentDf.to_excel('In_Documents.xlsx')
