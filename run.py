0db0e1a21ad7f18aed6e5e20a03914824696c98e
yegorkowalew <kowalew.backup@gmail.com>
2020-01-31 19:34:05 +0200

    файлы в папку testfiles

--- run.py
+++ run.py
@@ -10,10 +10,10 @@ from settings import READY_FILE, SN_FILE, IN_DOCUMENT_FILE, IN_DOCUMENT_FOLDER
 
 if __name__ == "__main__":
     readyDf = readyReadFile(READY_FILE)
-    readyDf.to_excel("Ready.xlsx")
+    readyDf.to_excel("testfiles\\Ready.xlsx")
 
     serviceNoteDf = serviceNoteReadFile(SN_FILE)
-    serviceNoteDf.to_excel("Service_Notes.xlsx")
+    serviceNoteDf.to_excel("testfiles\\Service_Notes.xlsx")
 
     inDocumentDf = worker(IN_DOCUMENT_FILE, IN_DOCUMENT_FOLDER)
-    inDocumentDf.to_excel('In_Documents.xlsx')
+    inDocumentDf.to_excel('testfiles\\In_Documents.xlsx')
