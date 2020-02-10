aea9071c0783c49671db072df633f6ce5ee1c02b
Коваленко Ігор Сергійович <C:\Users\i.kovalenko\AppData\Roaming\The Bat!>
2020-02-10 10:32:11 +0200

    Начало работ перезаписывает СЗ

--- appenddata.py
+++ appenddata.py
@@ -65,8 +65,8 @@ def appendDataWorker(df):
     for index, row in islice(df.iterrows(), 2, None):
         if not pd.isnull(row['order_plan_start']):
             # Если не установлена дата СЗ, то запускать установку дней не будем
-            setup_work_plan(df, index, dates_base)
             setup_order_plan_start(df, index, dates_base)
+            setup_work_plan(df, index, dates_base)
             setup_material(df, index, dates_base)
             setup_shipment(df, index, dates_base)
         else:
