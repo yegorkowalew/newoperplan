7a11ac7e2cde34b9b2b544e336181512a19902b5
Коваленко Ігор Сергійович <C:\Users\i.kovalenko\AppData\Roaming\The Bat!>
2020-02-13 11:22:28 +0200

    Разделение заказов, выравнивание

--- colors.py
+++ colors.py
@@ -36,41 +36,43 @@ def colorsWrite(df):
     # df = df.style.set_properties(**{'background-color': 'yellow'})
     def letter_color(value):
         if value == 'Ц':
-            return 'color: #d6a300; background-color:#fff2c8'
+            return 'color: #d6a300; background-color:#fff2c8; text-align: center'
         elif value == 'М':
-            return 'color: #548235; background-color:#e2efda'
+            return 'color: #548235; background-color:#e2efda; text-align: center'
         elif value == 'К':
-            return 'color: #2f75b5; background-color:#ddebf7'
+            return 'color: #2f75b5; background-color:#ddebf7; text-align: center'
         elif value == 'З':
-            return 'color: #000000; background-color:#8ea9db'
+            return 'color: #000000; background-color:#8ea9db; text-align: center'
         elif value == 'КВ':
-            return 'color: #215967; background-color:#daeef3'
+            return 'color: #215967; background-color:#daeef3; text-align: center'
         elif value == 'ОС':
-            return 'color: #5b3151; background-color:#e4dfec'
+            return 'color: #5b3151; background-color:#e4dfec; text-align: center'
         elif value == 'X':
-            return 'color: #c05065; background-color:#ff8596'
+            return 'color: #c05065; background-color:#ff8596; text-align: center'
         elif value == '>':
-            return 'color: #963634; background-color:#fde9d9'
+            return 'color: #963634; background-color:#fde9d9; text-align: center'
         elif value == 'СЗ':
-            return 'color: #f0800a; background-color:#fde9d9'
+            return 'color: #f0800a; background-color:#fde9d9; text-align: center'
         elif value == 'm':
-            return 'color: #a6afc0; background-color:#d9d9d9'
+            return 'color: #a6afc0; background-color:#d9d9d9; text-align: center'
         elif value == 'S':
-            return 'color: #ff7979; background-color:#ffafaf'
+            return 'color: #ff7979; background-color:#ffafaf; text-align: center'
         else:
             return 'color:black'
-        # elif value == 'М':
-        #     color = 'green'
-        # elif value == 'К':
-        #     color = 'black'
 
-        # return 'color: %s' % color
-
+    def highlight_greaterthan_1(x):
+        if x['shop'] == 'ОС':
+            return ['border-bottom-width:1px; border-bottom-color:#0010a7' for v in x]
+        else:
+            return ['border-bottom-width:1px; border-bottom-color:#cacaca' for v in x]
 
 
     df = df.style \
-    .applymap(letter_color, subset=[303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 379, 380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419]) \
-    .format({'total_amt_usd_pct_diff': "{:.2%}"})
+    .applymap(letter_color) \
+    .format({'total_amt_usd_pct_diff': "{:.2%}"}) \
+    .set_properties(**{'font-size': '10pt; vertical-align: middle'}) \
+    .apply(highlight_greaterthan_1, axis=1) \
+    .hide_index()
     return df
 
 if __name__ == "__main__":
